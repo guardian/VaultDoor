14427228a9db0e839ffe712dcb14a07891d78a76
Dave Allison <david.allison@theguardian.com>
2022-01-17 15:37:45 +0000

    Rewrite of how duplicatesArray is constructed.

--- app/services/DuplicateFinderService.scala
+++ app/services/DuplicateFinderService.scala
@@ -54,24 +54,22 @@ class DuplicateFinderService @Inject()(userInfoCache:UserInfoCache)(implicit mat
   }
 
   case class FullDuplicateData(mxfsPath:String, duplicateNumber:Int, duplicatesData:Seq[CachedEntry])
-  case class AllDuplicateData(dupes_count:Int, item_count:Int, duplicates:Array[FullDuplicateData])
+  case class AllDuplicateData(dupes_count:Int, item_count:Int, duplicates:Seq[FullDuplicateData])
 
   def getDuplicateData(vaultId: String)  = {
     loadExistingContent(vaultId).map(results=>{
       val contentCache = new ExistingArchiveContentCache(results)
-      var duplicatesArray: Array[FullDuplicateData] = Array()
       val dupeCount = contentCache.dupesCount
       if (dupeCount > 0) {
         logger.warn(s"There are $dupeCount duplicated files in the archive")
-        contentCache.dupedPaths.foreach(dupe => {
-          logger.debug(s"${dupe._1}: ${dupe._2} copies")
-          val duplicatedItemData = contentCache.getAllForPath(dupe._1)
-          val duplicateDataThree = FullDuplicateData(dupe._1,dupe._2,duplicatedItemData)
-          duplicatesArray +:= duplicateDataThree
-        })
       } else {
         logger.info(s"No duplicates found.")
       }
+      val duplicatesArray = contentCache.dupedPaths.map(dupe=>{
+        logger.debug(s"${dupe._1}: ${dupe._2} copies")
+        val duplicatedItemData = contentCache.getAllForPath(dupe._1)
+        FullDuplicateData(dupe._1, dupe._2, duplicatedItemData)
+      }).toSeq
       logger.info(s"Got existing ${results.length} items in the vault")
       val duplicateDataToReturn = AllDuplicateData(dupes_count = contentCache.dupesCount, item_count = results.length, duplicates = duplicatesArray)
       duplicateDataToReturn
